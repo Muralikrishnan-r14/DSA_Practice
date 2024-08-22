8f81494e273275891bd4c89fb1e8cbc2666f61aa
MuralKrish <139755977+MuralKrish@users.noreply.github.com>
2024-08-22 17:26:14 +0530

    Mural

--- Array/Easy/Remove duplicates in the sorted array.py
+++ Array/Easy/Remove duplicates in the sorted array.py
@@ -9,3 +9,5 @@ def remdup(arr):
     return arr[:j]
 arr=[1,1,1,2,2,3,3,3,3,4,4]
 print(remdup(arr))
+# Time complexity O(N)
+# space complexity O(N) , want to achieve O(1) means avoid slicing bcz it creates a new list
