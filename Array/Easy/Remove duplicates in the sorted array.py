31ab6b4a3e1dc20d297c96dd49d613906574b168
MuralKrish <139755977+MuralKrish@users.noreply.github.com>
2024-08-22 17:19:15 +0530

    Mural

---
+++ Array/Easy/Remove duplicates in the sorted array.py
@@ -0,0 +1,11 @@
+# Removing duplicates in the sorted array
+def remdup(arr):
+    j=0
+    for i in range(len(arr)):
+        if arr[i]!=arr[i-1]:
+            arr[j]=arr[i]
+            j+=1
+
+    return arr
+arr=[1,1,1,2,2,3,3,3,3,4,4]
+print(remdup(arr))
