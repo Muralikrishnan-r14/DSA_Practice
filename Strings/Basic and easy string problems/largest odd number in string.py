591c9f80fa20375537e2d87c40a835fb31043a37
MuralKrish <139755977+MuralKrish@users.noreply.github.com>
2024-08-31 07:32:19 +0530

    Mural

--- Strings/Basic and easy string problems/largest odd number in string.py
+++ Strings/Basic and easy string problems/largest odd number in string.py
@@ -3,9 +3,13 @@
 
 def largestOddnumber(num):
     for i in range(len(num)-1,-1,-1):
+        # Check if the current digit is odd
         if num[i] in '13579':
+            # Return the substring from the start to this position
             return num[:i+1]
-    return""
+    # If no odd digit is found, return an empty string
+    return ""
+
 
 num = '35427'
 print(largestOddnumber(num))
