217e63a44cf88786a938523b9a491379b8e9986f
MuralKrish <139755977+MuralKrish@users.noreply.github.com>
2024-09-04 23:33:53 +0530

    Mural

---
+++ Strings/Basic and easy string problems/Isomorphic string.py
@@ -0,0 +1,34 @@
+# Input: s = "egg", t = "add"   Input: s = "foo", t = "bar"   Input: s = "paper", t = "title"
+# Output: true                  Output: false                 Output: true
+
+def is_isomorphic(s: str, t: str) -> bool:
+    if len(s) != len(t):
+        return False
+
+    # Dictionaries to store character mappings
+    s_to_t_mapping = {}
+    t_to_s_mapping = {}
+
+    for c1, c2 in zip(s, t):
+        if c1 in s_to_t_mapping:
+            if s_to_t_mapping[c1] != c2:
+                return False
+        else:
+            s_to_t_mapping[c1] = c2
+
+        if c2 in t_to_s_mapping:
+            if t_to_s_mapping[c2] != c1:
+                return False
+        else:
+            t_to_s_mapping[c2] = c1
+
+    return True
+
+# Example usage:
+s1 = "egg"
+s2 = "ahh"
+print(is_isomorphic(s1, s2))  # Output: True
+
+s1 = "foo"
+s2 = "boo"
+print(is_isomorphic(s1, s2))  # Output: False
---
+++ Strings/Basic and easy string problems/Longest common prefix.py
@@ -0,0 +1,12 @@
+def longestCommonPrefix(v):
+    ans = ""
+    v = sorted(v)
+    first = v[0]
+    last = v[-1]
+    for i in range(min(len(first), len(last))):
+        if (first[i] != last[i]):
+            return ans
+        ans += first[i]
+    return ans
+v = ["flower","flow","flight"]
+print(longestCommonPrefix(v))
